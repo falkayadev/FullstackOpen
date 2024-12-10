f353acc42199b6add56df4c088ecfa68196707bd
Furkan Alkaya <falkaya.dev@gmail.com>
2024-12-10 16:55:13 +0300

    static folder comment removed

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
-// app.use(express.static("dist"));
+app.use(express.static("dist"));
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 });
