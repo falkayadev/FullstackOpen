b906c6562e51afc33c4be2b03bdd6df3342ff84f
Furkan Alkaya <falkaya.dev@gmail.com>
2024-12-11 15:12:25 +0300

    4.4 initial

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,11 @@ const dummy = (blogs) => {
   return 1;
 };
 
-export default { dummy };
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+  return blogs.reduce(reducer, 0);
+};
+
+export default { dummy, totalLikes };
