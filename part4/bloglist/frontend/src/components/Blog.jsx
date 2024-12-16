2e7f55edbee04cab6ae3716f91334db5d33a93ad
Furkan Alkaya <falkaya.dev@gmail.com>
2024-12-17 01:55:50 +0300

    5.7 completed

--- part4/bloglist/frontend/src/components/Blog.jsx
+++ part4/bloglist/frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const blogRow = { display: "flex", gap: "10px" };
+  return (
+    <div style={blogStyle}>
+      <div style={blogRow}>
+        <p>
+          {blog.title} by {blog.author}
+        </p>
+        <button
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+          }}
+        >
+          {isExpanded ? "hide" : "view"}
+        </button>
+      </div>
+      {isExpanded && (
+        <>
+          <p>{blog.url}</p>
+          <div style={blogRow}>
+            <p>
+              likes <span>{blog.likes}</span>
+            </p>
+            <button onClick={() => console.log("Liked!")}>like</button>
+          </div>
+          <p>{blog.author}</p>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
