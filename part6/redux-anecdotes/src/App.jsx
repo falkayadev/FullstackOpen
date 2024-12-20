e672905eab0f994ec64568d89358395ef3b50652
Furkan Alkaya <falkaya.dev@gmail.com>
2024-12-21 01:24:11 +0300

    6.14 and 6.15 created

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,22 @@
-import AnecdoteForm from "./components/AnecdoteForm";
-import AnecdoteList from "./components/AnecdoteList";
-import FilterForm from "./components/FilterForm";
-import Notification from "./components/Notification";
-import { useSelector } from "react-redux";
+import { useEffect } from 'react'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+import FilterForm from './components/FilterForm'
+import Notification from './components/Notification'
+import { useSelector, useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const notification = useSelector((state) => state.notification);
+  const notification = useSelector((state) => state.notification)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)))
+  }, [])
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -14,7 +25,7 @@ const App = () => {
       <AnecdoteList />
       <AnecdoteForm />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,17 +1,22 @@
-import { useDispatch } from "react-redux";
-import { create } from "../reducers/anecdoteReducer";
-import useNotification from "../hooks/useNotification";
-
+import { useDispatch } from 'react-redux'
+import { append } from '../reducers/anecdoteReducer'
+import useNotification from '../hooks/useNotification'
+import anecdoteService from '../services/anecdotes'
 const AnecdoteForm = () => {
-  const dispatch = useDispatch();
-  const notify = useNotification();
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const content = event.target.content.value;
-    event.target.content.value = "";
-    dispatch(create(content));
-    notify(`you created '${content}'`, dispatch);
-  };
+  const dispatch = useDispatch()
+  const notify = useNotification()
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const content = event.target.content.value
+    event.target.content.value = ''
+    anecdoteService
+      .createNew(content)
+      .then((data) => {
+        dispatch(append(data))
+        notify(`you created '${content}'`, dispatch)
+      })
+      .catch(() => notify(`Anecdote creation error!`, dispatch))
+  }
   return (
     <>
       <h2>create new</h2>
@@ -22,7 +27,7 @@ const AnecdoteForm = () => {
         <button>create</button>
       </form>
     </>
-  );
-};
+  )
+}
 
-export default AnecdoteForm;
+export default AnecdoteForm
