a476cd490ad7289c7b071034abf043b803d691b0
Furkan Alkaya <falkaya.dev@gmail.com>
2024-12-22 02:12:46 +0300

    6.22 completed

--- part6/redux-anecdotes_query/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes_query/src/components/AnecdoteList.jsx
@@ -1,11 +1,30 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from '../requests.js'
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from '../requests.js'
 
 const AnecdoteList = () => {
   const { data: anecdotes } = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
   })
+  const queryClient = useQueryClient()
+  const voteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(
+        ['anecdotes'],
+        anecdotes.map((a) =>
+          a.id !== updatedAnecdote.id ? a : updatedAnecdote
+        )
+      )
+    },
+  })
+
+  const handleVote = (id) => {
+    const anecdote = anecdotes.find((a) => a.id === id)
+    const updatedAnecdote = { ...anecdote, votes: anecdote.votes + 1 }
+    voteMutation.mutate({ id: id, newData: updatedAnecdote })
+  }
 
   return (
     <>
@@ -14,7 +33,7 @@ const AnecdoteList = () => {
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button>vote</button>
+            <button onClick={() => handleVote(anecdote.id)}>vote</button>
           </div>
         </div>
       ))}
--- part6/redux-anecdotes_query/src/requests.js
+++ part6/redux-anecdotes_query/src/requests.js
@@ -11,3 +11,8 @@ export const createAnecdote = async (newData) => {
   const response = await axios.post(baseUrl, newData)
   return response.data
 }
+
+export const updateAnecdote = async ({ id, newData }) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newData)
+  return response.data
+}
